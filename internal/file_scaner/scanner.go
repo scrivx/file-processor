09ce1268a932f5d619c0d3c34e682cd197bcbc9c
c4rlo5Riv3ra <jcriveraguadalupe@gmail.com>
2025-07-19 02:33:07 -0500

    add: scanner file

---
+++ internal/file_scaner/scanner.go
@@ -0,0 +1,22 @@
+package filescaner
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func ScanDir(dirPath string, fileChan chan<- string) error {
+	entries, err := os.ReadDir(dirPath)
+	if err != nil {
+		return fmt.Errorf("No se pudo leer el directorio %s", err)
+	}
+
+	for _, entry := range entries {
+		if !entry.IsDir() {
+			fullPath := filepath.Join(dirPath, entry.Name())
+			fileChan <- fullPath
+		}
+	}
+	return nil
+}
